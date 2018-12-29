365d152fa2c6698ff652adc1ab1ce8f6fad94578
Mathew Sherry <mjsherry248@gmail.com>
2018-12-29 13:37:00 +0100

    Initial Commit. Two scenes

---
+++ app/src/main/java/com/example/mjshe/rockclimbingroutemapper/CreateRoute.java
@@ -0,0 +1,13 @@
+package com.example.mjshe.rockclimbingroutemapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateRoute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_route);
+    }
+}
